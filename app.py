ff9ca6ab36070da7c382ca6ca95d08281a6e6168
Timothy Thomas <timothythomas.tt60@gmail.com>
2023-08-13 21:38:37 -0400

    AppDeploy

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, jsonify
-#import pickle
 import joblib
 import sys
 
 app = Flask(__name__)
 
-#Load pipeline
+# Load pipeline
 with open('full_pipeline.pkl', 'rb') as pkl:
-    pipeline=joblib.load(pkl)
+    pipeline = joblib.load(pkl)
 
 @app.route('/')
 def index():
@@ -15,107 +14,74 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Extract features from form
-    year = request.form['YEAR']
-    street1 = request.form['STREET1']
-    street2 = request.form['STREET2']
-    road_class = request.form['ROAD_CLASS']
-    district = request.form['DISTRICT']
-    loccoord = request.form['LOCCOORD']
-    traffctl = request.form['TRAFFCTL']
-    visibility = request.form['VISIBILITY']
-    light = request.form['LIGHT']
-    rdsfcond = request.form['RDSFCOND']
-    impactype = request.form['IMPACTYPE']
-    invtype = request.form['INVTYPE']
-    invage = request.form['INVAGE']
-    vehtype = request.form['VEHTYPE']
-    manoeuver = request.form['MANOEUVER']
-    drivact = request.form['DRIVACT']
-    drivcond = request.form['DRIVCOND']
-    pedtype = request.form['PEDTYPE']
-    pedact = request.form['PEDACT']
-    pedcond = request.form['PEDCOND']
-    cyclistype = request.form['CYCLISTYPE']
-    cycact = request.form['CYCACT']
-    cyccond = request.form['CYCCOND']
-    pedestrian = request.form['PEDESTRIAN']
-    cyclist = request.form['CYCLIST']
-    automobile = request.form['AUTOMOBILE']
-    motorcycle = request.form['MOTORCYCLE']
-    truck = request.form['TRUCK']
-    trsn_city_veh = request.form['TRSN_CITY_VEH']
-    emerg_veh = request.form['EMERG_VEH']
-    passenger = request.form['PASSENGER']
-    speeding = request.form['SPEEDING']
-    ag_driv = request.form['AG_DRIV']
-    redlight = request.form['REDLIGHT']
-    alcohol = request.form['ALCOHOL']
-    disability = request.form['DISABILITY']
-    neighbourhood_158 = request.form['NEIGHBOURHOOD_158']
-    month = request.form['MONTH']
-    day_of_week = request.form['DAY_OF_WEEK']
-    is_rush_hr = request.form['IS_RUSH_HR']
- 
+    data = request.get_json()
+    
+    # Extract features from data
+    year = data['YEAR'][0]
+    street1 = data['STREET1'][0]
+    street2 = data['STREET2'][0]
+    road_class = data['ROAD_CLASS'][0]
+    district = data['DISTRICT'][0]
+    loccoord = data['LOCCOORD'][0]
+    traffctl = data['TRAFFCTL'][0]
+    visibility = data['VISIBILITY'][0]
+    light = data['LIGHT'][0]
+    rdsfcond = data['RDSFCOND'][0]
+    impactype = data['IMPACTYPE'][0]
+    invtype = data['INVTYPE'][0]
+    invage = data['INVAGE'][0]
+    vehtype = data['VEHTYPE'][0]
+    manoeuver = data['MANOEUVER'][0]
+    drivact = data['DRIVACT'][0]
+    drivcond = data['DRIVCOND'][0]
+    pedtype = data['PEDTYPE'][0]
+    pedact = data['PEDACT'][0]
+    pedcond = data['PEDCOND'][0]
+    cyclistype = data['CYCLISTYPE'][0]
+    cycact = data['CYCACT'][0]
+    cyccond = data['CYCCOND'][0]
+    neighbourhood_158 = data['NEIGHBOURHOOD_158'][0]
+    month = data['MONTH'][0]
+    day_of_week = data['DAY_OF_WEEK'][0]
+    is_rush_hr = data['IS_RUSH_HR'][0]
 
-
-
-    data = {
-    'YEAR': [year],
-    'STREET1': [street1],
-    'STREET2': [street2],
-    'ROAD_CLASS': [road_class],
-    'DISTRICT': [district],
-    'LOCCOORD': [loccoord],
-    'TRAFFCTL': [traffctl],
-    'VISIBILITY': [visibility],
-    'LIGHT': [light],
-    'RDSFCOND': [rdsfcond],
-    'IMPACTYPE': [impactype],
-    'INVTYPE': [invtype],
-    'INVAGE': [invage],
-    'VEHTYPE': [vehtype],
-    'MANOEUVER': [manoeuver],
-    'DRIVACT': [drivact],
-    'DRIVCOND': [drivcond],
-    'PEDTYPE': [pedtype],
-    'PEDACT': [pedact],
-    'PEDCOND': [pedcond],
-    'CYCLISTYPE': [cyclistype],
-    'CYCACT': [cycact],
-    'CYCCOND': [cyccond],
-    'PEDESTRIAN': [pedestrian],
-    'CYCLIST': [cyclist],
-    'AUTOMOBILE': [automobile],
-    'MOTORCYCLE': [motorcycle],
-    'TRUCK': [truck],
-    'TRSN_CITY_VEH': [trsn_city_veh],
-    'EMERG_VEH': [emerg_veh],
-    'PASSENGER': [passenger],
-    'SPEEDING': [speeding],
-    'AG_DRIV': [ag_driv],
-    'REDLIGHT': [redlight],
-    'ALCOHOL': [alcohol],
-    'DISABILITY': [disability],
-    'NEIGHBOURHOOD_158': [neighbourhood_158],
-    'MONTH': [month],
-    'DAY_OF_WEEK': [day_of_week],
-    'IS_RUSH_HR': [is_rush_hr]
-}
-
+    # Prepare the data for prediction
+    x = {
+        'YEAR': year,
+        'STREET1': street1,
+        'STREET2': street2,
+        'ROAD_CLASS': road_class,
+        'DISTRICT': district,
+        'LOCCOORD': loccoord,
+        'TRAFFCTL': traffctl,
+        'VISIBILITY': visibility,
+        'LIGHT': light,
+        'RDSFCOND': rdsfcond,
+        'IMPACTYPE': impactype,
+        'INVTYPE': invtype,
+        'INVAGE': invage,
+        'VEHTYPE': vehtype,
+        'MANOEUVER': manoeuver,
+        'DRIVACT': drivact,
+        'DRIVCOND': drivcond,
+        'PEDTYPE': pedtype,
+        'PEDACT': pedact,
+        'PEDCOND': pedcond,
+        'CYCLISTYPE': cyclistype,
+        'CYCACT': cycact,
+        'CYCCOND': cyccond,
+        'NEIGHBOURHOOD_158': neighbourhood_158,
+        'MONTH': month,
+        'DAY_OF_WEEK': day_of_week,
+        'IS_RUSH_HR': is_rush_hr
+    }
 
     # Make the prediction
-    prediction = pipeline.predict(data)
+    prediction = pipeline.predict([x])
 
     # Return the prediction to the user
     return jsonify({'prediction': prediction[0]})
 
-if __name__ == '__main__':
-    try:
-        port=int(sys.argv[1])
-
-    except:
-        port=8000
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
-    app.run(debug=True)
